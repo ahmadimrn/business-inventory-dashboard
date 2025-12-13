ff5c1f18804189fe41122bbbcf7ef3b06b092c0f
Ahmad Imran <ahmadimran2728@gmail.com>
2025-12-13 11:38:08 +0500

    Formatted index routes file

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import productRoutes from './product.routes.js';
-import importRoutes from './import.routes.js';
+import productRoutes from "./product.routes.js";
+import importRoutes from "./import.routes.js";
+import userRoutes from "./user.routes.js";
 
 const router = Router();
-router.use('/products', productRoutes);
-router.use('/import', importRoutes);
+router.use("/products", productRoutes);
+router.use("/import", importRoutes);
+router.use("/user", userRoutes);
 
 export default router;
