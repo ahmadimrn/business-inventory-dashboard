3c8991a0d7ff74d85c3ba298e8bf146b37c7dde8
Ahmad Imran <ahmadimran2728@gmail.com>
2025-12-13 11:35:21 +0500

    Add user routes

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,8 @@
+import { application, Router } from "express";
+import * as controller from "../controllers/user.controller.js";
+
+const router = Router();
+
+router.post("/", controller.createUser);
+
+export default router;
