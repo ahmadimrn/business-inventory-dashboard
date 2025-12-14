18466cfd8ca906678d18530909fed6891ddaf6cf
Ahmad Imran <ahmadimran2728@gmail.com>
2025-12-14 22:50:34 +0500

    Add functionality for dynamic user_id

--- backend/src/services/product.service.js
+++ backend/src/services/product.service.js
@@ -10,12 +10,12 @@ export const getAll = async (user_id) => {
     return getAll;
 };
 
-export const create = async (data) => {
-    const { user_id, name, price, stock, description } = data;
+export const create = async (req) => {
+    const { name, price, stock, description } = req.body;
 
     const newProduct = await prisma.product.create({
         data: {
-            user_id,
+            user_id: req.user.id,
             name,
             price,
             stock,
