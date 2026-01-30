41c7f0cbb3f41de427cf7a2a69c8116c214b16df
Ahmad Imran <ahmadimran2728@gmail.com>
2026-01-30 15:42:29 +0500

    Fix cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,9 +11,9 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
         credentials: true,
-    })
+    }),
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
